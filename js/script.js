a407f86d71b1f08b80544aa114db3347aca8411d
Md. Amir Hamza <shuvokhan2044@gmail.com>
2020-06-26 22:15:05 +0600

    bar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+    $(window).on('load',function(){
+        $('.bar').animate({width: '100%'}, 3000);
+    });
+    $(window).on('load',function(){
+        $('.bar-two').animate({width: '100%'}, 3000);
+    });
+    $(window).on('load',function(){
+        $('.bar-three').animate({width: '80%'}, 3000);
+    });
+    $(window).on('load',function(){
+        $('.bar-four').animate({width: '90%'}, 3000);
+    });
+    $(window).on('load',function(){
+        $('.bar-five').animate({width: '100%'}, 3000);
+    });
+    $(window).on('load',function(){
+        $('.bar-six').animate({width: '70%'}, 3000);
+    });
+    $(window).on('load',function(){
+        $('.bar-seven').animate({width: '60%'}, 3000);
+    });
+});
